7bd96de59aca28104da1d0231805d6d9ef03fb6d
juuurr <juuurr@naver.com>
2024-10-05 21:35:53 +0900

    개발용과 배포용 도커 파일 구분

--- ver1/blog/urls.py
+++ ver1/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
     #path('', views.index),  # blog/ 경로로 접속할 때 index 뷰를 실행
 ]

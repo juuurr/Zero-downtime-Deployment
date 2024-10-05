e9818bcd7a925bc7c4106b43899b5ab9a95a9229
juuurr <juuurr@naver.com>
2024-10-05 21:02:11 +0900

    create basic and make post list by CBV

--- ver1/project1/urls.py
+++ ver1/project1/urls.py
@@ -20,5 +20,10 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('single_pages.urls')), #대문
 ]
+
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

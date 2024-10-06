ddae3fa5aa2a7bde78b90ece3eccef2d2828b950
juuurr <juuurr@naver.com>
2024-10-06 16:16:00 +0900

    Deploy by docer

--- blue/project1/settings.py
+++ blue/project1/settings.py
@@ -124,8 +124,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')

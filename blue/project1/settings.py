b203def648c38c074d0c2b5a45907984fdfadbb2
juuurr <juuurr@naver.com>
2024-10-06 14:44:53 +0900

    포스트 목록, 상세 페이지에 이미지 출력

--- blue/project1/settings.py
+++ blue/project1/settings.py
@@ -127,6 +127,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
